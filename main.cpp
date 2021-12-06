ff74c2c8c36767ad711bc53967773b4e7eb85beb
Sentyabrina Sulakova <sulakova.sentyabrina@yandex.ru>
2021-12-07 00:26:19 +0300

    Added bones: main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	char z;
+	double procent;
+
+	cout « "First number : ";
+	cin » a;
+	cout « "Second number: ";
+	cin » b;
+	cout « "Choose an action (+, -, * or /): ";
+	cin » z;
+	switch (z)
+	{
+	case '+':
+		cout « " + in progress";
+		break;
+
+	case '-':
+		cout « " - in progress";
+		break;
+
+	case "*":
+		cout « " * in progress";
+		break;
+
+	case '/':
+		cout « " / in progress";
+		break;
+
+
+	default: cout « "Wrong action";;
+	}
+
+	return 0;
+
+}
